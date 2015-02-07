c7967a0a6077cd28ed4df9eb1784d846898c4ef8
Kevin4theNguyen <kan02012@mymail.pomona.edu>
2015-02-06 16:45:20 -0800

    Initial commit

---
+++ ParseStarterProject/src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,32 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Initialize Crash Reporting.
+    ParseCrashReporting.enable(this);
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+      Parse.initialize(this, "9nWnCUTdcZrrXtlGQKOjgPJWayPRKyMSQzU2bXhX", "dCjilcjkIqYAlyx55CIwFqyVjzl1GvKAuML64sXo");
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    // Optionally enable public read access.
+    // defaultACL.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
